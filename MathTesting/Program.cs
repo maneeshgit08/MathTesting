105b200888e7aa2da6cab82b6e264291c8cdef2e
Kabra, Manish (CT RDA DS AA DTS AU APPS) <manish.kabra@siemens.com>
2021-06-14 15:57:38 +0530

    first commit

---
+++ MathTesting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathOperation;
+
+namespace MathTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c= new Class1();
+            ;
+            Console.WriteLine(c.Addition(3,5));
+            Console.WriteLine(c.Subtraction(3, 5));
+            Console.WriteLine(c.Multiplication(3, 5));
+            Console.Read();
+        }
+    }
+}
